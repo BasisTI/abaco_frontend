f2d3785a85cf022fffd3a0c2f9576932ea32180d
marlononorio <marlon.onorio@gmail.com>
2019-09-09 11:56:49 -0300

    adicionando metodo find

--- src/app/tipo/tipo.service.ts
+++ src/app/tipo/tipo.service.ts
@@ -2,13 +2,18 @@ import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Tipo } from './tipo-model/tipo.model';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class TipoService {
 
-  constructor(private http: Http) { }
+  url: String;
 
-  findAll(tipo: Tipo): Observable<Tipo> {
+  constructor(private http: Http) { 
+    this.url = `${environment.apiUrl}/tipo`;
+  }
+
+  find(tipo: Tipo): Observable<Tipo> {
     return null;
   }
 }
