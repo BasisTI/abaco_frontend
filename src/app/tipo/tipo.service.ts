62335b9b20a1e1a511536836605d0fb757442b6b
marlononorio <marlon.onorio@gmail.com>
2019-09-10 08:45:10 -0300

    adicionando metodos find,create e delete na classe tipo service

--- src/app/tipo/tipo.service.ts
+++ src/app/tipo/tipo.service.ts
@@ -1,4 +1,5 @@
 import { Observable } from 'rxjs/Observable';
+import { Response } from '@angular/http';
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Tipo } from './tipo-model/tipo.model';
@@ -9,11 +10,20 @@ export class TipoService {
 
   url: String;
 
-  constructor(private http: Http) { 
+  constructor(private http: Http) {
     this.url = `${environment.apiUrl}/tipo`;
   }
 
-  find(tipo: Tipo): Observable<Tipo> {
-    return null;
+  find(id: number): Observable<Tipo> {
+    return this.http.get(`${this.url}/${id}`)
+      .map(response => response.json());
+  }
+
+  create(tipo: Tipo): Observable<Response> {
+    return this.http.post(`${this.url}`, tipo);
+  }
+
+  delete(id: number): Observable<Response> {
+    return this.http.delete(`${this.url}/${id}`);
   }
 }
