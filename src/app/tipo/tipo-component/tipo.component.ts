8ec0fe116452ef8e99010905fb9c1d124fa04f45
marlononorio <marlon.onorio@gmail.com>
2019-09-10 09:56:46 -0300

    adicionando metodo getPage na classe tipo service

--- src/app/tipo/tipo-component/tipo.component.ts
+++ src/app/tipo/tipo-component/tipo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TipoService } from '../tipo.service';
 @Component({
   selector: 'app-tipo',
   templateUrl: './tipo.component.html',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TipoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private tipoService: TipoService) { }
 
   ngOnInit() {
   }
--- src/app/tipo/tipo.service.ts
+++ src/app/tipo/tipo.service.ts
@@ -1,14 +1,16 @@
+import { TipoFilter } from './tipo-model/tipo.filter';
 import { Observable } from 'rxjs/Observable';
 import { Response } from '@angular/http';
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Tipo } from './tipo-model/tipo.model';
 import { environment } from '../../environments/environment';
-
+import { DataTable } from 'primeng/primeng';
+import { RequestUtil } from '../util/requestUtil'
 @Injectable()
 export class TipoService {
 
-  url: String;
+  private url: String;
 
   constructor(private http: Http) {
     this.url = `${environment.apiUrl}/tipo`;
@@ -17,6 +19,13 @@ export class TipoService {
   find(id: number): Observable<Tipo> {
     return this.http.get(`${this.url}/${id}`)
       .map(response => response.json());
+  }
+
+  getPage(filtro :TipoFilter, dataTable: DataTable): Observable<Response>{
+    const optionsParam = {
+      params: RequestUtil.getRequestParams(dataTable)
+    }
+    return this.http.post(`${this.url}/page`, filtro, optionsParam);
   }
 
   create(tipo: Tipo): Observable<Response> {
