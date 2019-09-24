92076c6928288e9b574cbe5d072ba2a025dd4387
marlononorio <marlon.onorio@gmail.com>
2019-09-24 08:59:00 -0300

    Adicionando mensagem de erro para registros com nome ja existente

--- src/app/shared/constants/errorConstants.ts
+++ src/app/shared/constants/errorConstants.ts
@@ -2,5 +2,6 @@ export enum ErrorConstants {
     fase_cadastrada = 'Global.Mensagens.RegistroCadastrado',
     fase_em_uso = 'Cadastros.Fase.Mensagens.msgNaoEPossivelExcluirRegistrosVinculados',
 
-    erro_gerar_relatorio = 'Global.Mensagens.ErrorExportar'
+    erro_gerar_relatorio = 'Global.Mensagens.ErrorExportar',
+    tipo_cadastrado = 'Global.Mensagens.RegistroCadastrado'
 }
