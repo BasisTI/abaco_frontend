570eb0f0a2f4065488f41f34f5655a505f5e93ec
eduardo.morais <eduardo.morais@basis.com.br>
2020-10-14 10:52:02 -0300

    Atualizando imagem docker BASIS-152771

--- docker/nginx/build.sh
+++ docker/nginx/build.sh
@@ -5,12 +5,9 @@ set -eu
 npm --version
 
 npm install
-npm run build -- --prod
-#npm run build
+#npm run build -- --prod
+npm run build
 
-cd ../../dist/
+cd dist/
 tar zcvf dist.tar.gz *
-mv dist.tar.gz ../docker/nginx/
-
-cd ../docker/nginx
-chmod -R a+x *.sh
+mv dist.tar.gz /usr/share/nginx/html/
